77a45e8842de75c8659bf4d00d23e15a62df52de
Andrea Becerra <andreadeleon729@gmail.com>
2022-04-29 17:17:30 -0600

    eliminando productos

---
+++ src/containers/productList.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import Tables from '../components/table'
+
+
+// const ProductList = () => {
+//     return (
+//          <Tables/>
+//     );
+// }
+
+// export default ProductList;
